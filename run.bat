a4957cf2829b67ee7f390d099b95f2844397acb4
Kenneth Chen <92031860+kenJPG@users.noreply.github.com>
2023-04-10 14:52:18 +0800

    f

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 set PATH=%path%;C:\MinGW\bin;
+g++ cpp_modules\src\main.cpp -o cpp_modules\src\main.exe
 python -m venv tetris_env
 CALL tetris_env/Scripts/activate.bat
 python -m pip install --upgrade pip
